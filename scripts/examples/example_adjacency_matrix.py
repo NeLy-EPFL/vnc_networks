d9cb77b997a7cbe3742f6e4c69f31d9069f9d191
fhurtak <femke.hurtak@gmail.com>
2024-09-11 15:15:23 +0200

    adding example script

---
+++ scripts/examples/example_adjacency_matrix.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+
+import specific_neurons.mdn_helper as mdn_helper
+
+if __name__ == '__main__':
+    neuropil = 'hl'
+    side = 'RHS'
+
+    # Load the VNC network
+    VNC = mdn_helper.get_vnc_split_MDNs_by_neuropil(
+        not_connected=mdn_helper.get_mdn_bodyids()
+        )
+    
+    # Identify specific neurons
+    source_neurons = mdn_helper.get_subdivided_mdns(VNC, neuropil, side)
+    target_dict =  { # right hind leg motor neurons
+        'class:string': 'motor neuron',
+        'somaSide:string': side,
+        'subclass:string': neuropil,
+        }
+    target_neurons = VNC.get_neuron_ids(target_dict)
+    
+    # Get the graph made of neurons contacting MDNs -> motor neurons
+    # within n hops
+    n_hops = 2
+    graph = VNC.paths_length_n(n_hops, source_neurons, target_neurons)
+
+    # Get the graph with all existing connections between the interesting neurons
+    # (not only the shortest paths)
+    subconnections = VNC.subgraph(
+        graph.nodes,
+        )  # new Connections object
+    
+    # Get the adjacency matrix only
+    adjacency_matrix = subconnections.get_adjacency_matrix()
+
+    # Get the adjacency matrix with tracking of the neurons names
+    cmatrix = subconnections.get_cmatrix(type_ = "syn_count")
+    print(cmatrix.get_lookup().head())
+    cmatrix.imshow()
+    plt.savefig('adjacency_matrix.png')
