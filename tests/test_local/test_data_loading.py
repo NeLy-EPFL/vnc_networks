46efd3d9a153b832a916e9d255c957855c693642
Dom <dominicd7@hotmail.com>
2025-02-21 17:28:28 +0100

    Add basic tests for MANC v1.2 connections

--- tests/test_local/test_data_loading.py
+++ tests/test_local/test_data_loading.py
@@ -31,3 +31,29 @@ class TestDataLoading:
 
         df_2 = df[(df["start_bid"] == 10001) & (df["end_bid"] == 29119)]
         assert df_2["eff_weight"].values[0] == -5, "Incorrect nt_type handling"
+
+    def test_connections_instantiation_MANCv1_2(self):
+        """
+        Test the instantiation of the Connections class.
+        """
+        import vnc_networks
+        from vnc_networks.connections import Connections
+
+        # Instantiate a Connections object
+        valid_connections = Connections(
+            vnc_networks.connectome_reader.MANC_v_1_2("v1.2")
+        )
+        assert valid_connections is not None, "Connections object not instantiated"
+        cr_name = valid_connections.CR.connectome_name
+        assert cr_name == "manc", "Incorrect connectome name"
+        cr_version = valid_connections.CR.connectome_version
+        assert cr_version == "v1.2", "Incorrect connectome version"
+
+        # check the data loading
+        df = valid_connections.get_dataframe()
+
+        df_1 = df[(df["start_bid"] == 11218) & (df["end_bid"] == 10094)]
+        assert df_1["eff_weight"].values[0] == 915, "Incorrect data values"
+
+        df_2 = df[(df["start_bid"] == 10725) & (df["end_bid"] == 10439)]
+        assert df_2["eff_weight"].values[0] == -1080, "Incorrect nt_type handling"
