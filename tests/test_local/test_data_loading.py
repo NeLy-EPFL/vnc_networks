381e0af911bf3aaf8090db8e7dcc39a0a0a68ca8
Dom <dominicd7@hotmail.com>
2025-02-21 10:05:05 +0100

    Connectome name is lowercase but test was expecting uppercase

--- tests/test_local/test_data_loading.py
+++ tests/test_local/test_data_loading.py
@@ -9,17 +9,17 @@ class TestDataLoading:
     
     def test_connections_instantiation(self):
         """
-        Test the instanciation of the Connections class.
+        Test the instantiation of the Connections class.
         """
         from vnc_networks.connections import Connections
 
-        # Instanciate a Connections object
+        # Instantiate a Connections object
         valid_connections = Connections()
-        assert valid_connections is not None, "Connections object not instanciated"
+        assert valid_connections is not None, "Connections object not instantiated"
 
         # default associated ConnectomeReader should be MANC v1.0
         cr_name = valid_connections.CR.connectome_name
-        assert cr_name == 'MANC', "Incorrect connectome name"
+        assert cr_name == "manc", "Incorrect connectome name"
         cr_version = valid_connections.CR.connectome_version
         assert cr_version == 'v1.0', "Incorrect connectome version"
 
@@ -29,5 +29,5 @@ class TestDataLoading:
         df_1 = df[(df['start_bid'] == 10000) & (df['end_bid'] == 14882)]
         assert df_1['eff_weight'].values[0] == 136, "Incorrect data values"
 
-        df_2 = df[(df['start_bid'] == 10001) & (df['end_bid'] == 29119)]
-        assert df_2['eff_weight'].values[0] == -5, "Incorrect nt_type handeling"
+        df_2 = df[(df["start_bid"] == 10001) & (df["end_bid"] == 29119)]
+        assert df_2["eff_weight"].values[0] == -5, "Incorrect nt_type handling"
