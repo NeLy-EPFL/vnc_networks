ddc18c4eeb0a1ba0de42c76b2f7ae2b2d2a49661
fhurtak <femke.hurtak@gmail.com>
2025-02-25 17:26:00 +0100

    handeling synapse ids that are not unique

--- tests/test_local/test_data_loading.py
+++ tests/test_local/test_data_loading.py
@@ -1,33 +1,35 @@
-'''
+"""
 Can only run when the data is available. Not run in the CI.
-'''
+"""
+
 
 class TestDataLoading:
     """
     Test the data loading functions.
     """
-    
+
     def test_connections_instantiation(self):
         """
         Test the instantiation of the Connections class.
         """
         from vnc_networks.connections import Connections
+        from vnc_networks.connectome_reader import MANC
 
         # Instantiate a Connections object
-        valid_connections = Connections()
+        valid_connections = Connections(CR=MANC("v1.0"))
         assert valid_connections is not None, "Connections object not instantiated"
 
         # default associated ConnectomeReader should be MANC v1.0
         cr_name = valid_connections.CR.connectome_name
         assert cr_name == "manc", "Incorrect connectome name"
         cr_version = valid_connections.CR.connectome_version
-        assert cr_version == 'v1.0', "Incorrect connectome version"
+        assert cr_version == "v1.0", "Incorrect connectome version"
 
         # check the data loading
         df = valid_connections.get_dataframe()
 
-        df_1 = df[(df['start_bid'] == 10000) & (df['end_bid'] == 14882)]
-        assert df_1['eff_weight'].values[0] == 136, "Incorrect data values"
+        df_1 = df[(df["start_bid"] == 10000) & (df["end_bid"] == 14882)]
+        assert df_1["eff_weight"].values[0] == 136, "Incorrect data values"
 
         df_2 = df[(df["start_bid"] == 10001) & (df["end_bid"] == 29119)]
         assert df_2["eff_weight"].values[0] == -5, "Incorrect nt_type handling"
